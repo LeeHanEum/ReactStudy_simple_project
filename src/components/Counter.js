8b594cd90bdda4bcf2c13c11676f089c51a7878a
LeeHanEum <LeeHanEum160924@outlook.kr>
2023-11-14 22:04:19 +0900

    feat : userInfo 추가

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import PrimeNum from "./PrimeNum";
 import {Button} from "react-bootstrap";
 import "../styles/style.css"
